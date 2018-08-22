3fa5558ee359ae1b679a1d5d6bf50f450e0cd20f
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 13:54:35 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,6 +2,8 @@ def get_first_name_of_season_winner(data, season)
   data[season.to_s].each do |contestant|
    if contestant[:status] == "Winner"
      return contestant[:name].split(" ")[0]
+   else
+     puts "No winner,"
     end
   end
 end
