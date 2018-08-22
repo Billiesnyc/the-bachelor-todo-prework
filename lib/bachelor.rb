208c546eb8faff95fe50eef3d3b70843c00028ef
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 14:06:31 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,5 @@
 def get_first_name_of_season_winner(data, season)
-  data.each do |season_hash, contestants_array|
-    if season_hash.to_s == season
-      contestants_array.each do |contestant|
-   if contestant["status"] == "Winner"
-     return contestant["name"].split(" ")[0]
-        end
-      end
-    end
-  end
+  
 end
 
 def get_contestant_name(data, occupation)
