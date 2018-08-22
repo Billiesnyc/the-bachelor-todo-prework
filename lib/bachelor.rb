6c3290f377698d7e4554c0a1b7a1eef2b42b7f90
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 14:21:15 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_hash, contestants_array|
+    if season_hash.to_s == season
+      contestants_array.each do |contestant|
+   if contestant["status"] == "Winner"
+     return contestant["name"].split(" ")[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_hash, contestants_array|
+    contestants_array.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+   count = 0
+   data.each do |season_hash, contestants_array|
+     contestants_array.each do |contestant|
+       if contestant["hometown"] == hometown
+         count += 1
+       end
+     end
+   end
+   count
 end
 
 def get_occupation(data, hometown)
-  # code here
+ data.each do |season_hash, contestants_array|
+     contestants_array.each do |contestant|
+       if contestant["hometown"] == hometown
+         return contestant["occupation"]
+       end
+     end
+   end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+   age_total = 0 
+   number_of_contestants = 0 
+   data.each do |season_hash, contestants_array|
+    if season_hash.to_s == season
+      contestants_array.each do |contestant|
+        number_of_contestants += 1 
+        age_total += contestant["age"].to_i
+      end
+    end
+  end
+  average_age = age_total.to_f / number_of_contestants.to_f
+  average_age.to_s[3].to_i >= 5 ? average_age.ceil : average_age.to_i
 end
