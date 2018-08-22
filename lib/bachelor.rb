85d6c83d3a2a64bb668fcf463e3d149afa2e67b0
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 14:11:04 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,6 +37,9 @@ def get_occupation(data, hometown)
      contestants_array.each do |contestant|
        if contestant["hometown"] == hometown
          return contestant["occupation"]
+       end
+     end
+   end
 end
 
 def get_average_age_for_season(data, season)
