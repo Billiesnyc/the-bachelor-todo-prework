9c454fd68b7ffe0226b663550606259762163741
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 14:16:21 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -53,6 +53,6 @@ def get_average_age_for_season(data, season)
       end
     end
   end
-  average_age = number_of_contestants / age_total
+  average_age = age_total / number_of_contestants
   average_age
 end
