cf277f2ab453f1b05a498f12e283e887e3f36cd7
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 14:14:40 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_average_age_for_season(data, season)
     if season_hash.to_s == season
       contestants_array.each do |contestant|
         number_of_contestants += 1 
-        
+        age_total 
       end
     end
   end
